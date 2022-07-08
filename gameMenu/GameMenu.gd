5832ecf68cb3760d2533ddaa6acc4c910d68d4ef
Ivan Pong <g.n.nop15@gmail.com>
2022-07-07 23:35:56 -0500

    indicative code for menu

---
+++ gameMenu/GameMenu.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func check():
+	print("Hey Jude")
+	#instead of calling print, initializ the game obj here.
+	#This is a multi stage process. First, 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ gameMenu/NewGameButton.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#Now, we can get the scene, and attach it to the top node:
+
+
+func _on_button_up():
+	print("Hello world!")
+	get_node("/root/GameMenu").check()
