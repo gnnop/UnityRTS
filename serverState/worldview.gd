b8481fab665c15a43f7b7e4ec25c3a1707e6683e
gnnop <gavin.nop@gmail.com>
2022-08-16 01:01:15 -0500

    Yes, I cheated, zoom ability added

---
+++ serverState/worldview.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+
+var zoom_factor = 1.1
+
+func _input(event):
+	if event is InputEventMouse:
+		if event.is_pressed() and not event.is_echo():
+			var mouse_position = event.position
+			if event.button_index == BUTTON_WHEEL_UP:
+				_zoom_at_point(zoom_factor, mouse_position)
+			elif event.button_index == BUTTON_WHEEL_DOWN:
+				_zoom_at_point(1 / zoom_factor, mouse_position)
+
+
+func _zoom_at_point(zoom_change, mouse_position):
+	scale = scale * zoom_change
+	var delta_x = (mouse_position.x - global_position.x) * (zoom_change - 1)
+	var delta_y = (mouse_position.y - global_position.y) * (zoom_change - 1)
+	global_position.x = global_position.x - delta_x
+	global_position.y = global_position.y - delta_y
