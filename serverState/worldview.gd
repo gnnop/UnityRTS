14f3818f9aba32dc3dbe62332a1e26deed30a2a8
gnnop <gavin.nop@gmail.com>
2022-08-16 01:37:45 -0500

    mouse movement finished. Next, terrain generation

--- serverState/worldview.gd
+++ serverState/worldview.gd
@@ -2,15 +2,23 @@ extends Camera2D
 
 
 var zoom_factor = 1.1
+var is_mouse_pressed = false
+var stored_mouse_pos = Vector2(0.0, 0.0)
 
 func _input(event):
 	if event is InputEventMouse:
-		if event.is_pressed() and not event.is_echo():
+		if event.is_action_type() and not event.is_echo():
 			var mouse_position = event.position
 			if event.button_index == BUTTON_WHEEL_UP:
 				_zoom_at_point(zoom_factor, mouse_position)
 			elif event.button_index == BUTTON_WHEEL_DOWN:
 				_zoom_at_point(1 / zoom_factor, mouse_position)
+			elif event.button_index == BUTTON_LEFT:
+				is_mouse_pressed = event.pressed
+		if event is InputEventMouseMotion and is_mouse_pressed:
+			_translate_screen(event.relative)
+			
+				
 
 
 func _zoom_at_point(zoom_change, mouse_position):
@@ -19,3 +27,9 @@ func _zoom_at_point(zoom_change, mouse_position):
 	var delta_y = (mouse_position.y - global_position.y) * (zoom_change - 1)
 	global_position.x = global_position.x - delta_x
 	global_position.y = global_position.y - delta_y
+
+
+func _translate_screen(rel_coords):
+	var v0 = get_viewport().size
+	global_position.x = global_position.x + rel_coords.x
+	global_position.y = global_position.y + rel_coords.y
